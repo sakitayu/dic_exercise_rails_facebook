c639171b94b065c8a0ad583a21fd625b8b19f79b
Yu Sakita <sakitakitakita@gmail.com>
2019-10-18 14:27:02 +0900

    画像を投稿しなかった場合にエラーが出る不具合を修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_081326) do
+ActiveRecord::Schema.define(version: 2019_10_18_051336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
-    t.string "title"
     t.text "content"
     t.bigint "user_id"
     t.datetime "created_at", null: false
